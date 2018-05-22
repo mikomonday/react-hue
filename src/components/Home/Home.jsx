032bfd648b46e222def7fafe8b43a2a35590d9ca
James Nguyen <miko.monday@gmail.com>
2018-05-21 21:10:24 -0700

    Added headline banner

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 export default class Home extends React.Component {
+  state = {};
   render() {
     return (
-      <div>hello :3</div>
+      <div className="home">
+        <Typography variant="headline">
+          Home
+        </Typography>
+      </div>
     );
   }
 }
