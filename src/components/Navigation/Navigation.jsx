74e1c81ad7b9be2efec6dd7645ac42a501b84350
James Nguyen <miko.monday@gmail.com>
2018-05-21 21:09:20 -0700

    Added avatar and settings icon

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
+
+// Material UI
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
+import Icon from '@material-ui/core/Icon';
+
+// Styles
+import './Navigation.scss';
 
 export default class Navigation extends React.Component {
   state = {};
 
   render() {
     return (
-      <AppBar position="static" color="default">
+      <AppBar classes={{ root: 'nav' }} elevation={0} position="static" color="default">
         <Toolbar>
+          <Avatar classes={{ root: 'nav-avatar' }}>
+            <Icon>person</Icon>
+          </Avatar>
           <Typography variant="title" color="inherit">
             React-Hue
           </Typography>
+          <IconButton classes={{ root: 'nav-settings' }} aria-label="Settings">
+            <Icon>tune</Icon>
+          </IconButton>
         </Toolbar>
       </AppBar>
     );
