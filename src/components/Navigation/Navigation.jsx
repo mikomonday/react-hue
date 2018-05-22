0d369985d16dc974bfe2d9b31dfbe67ea2fa5b2c
James Nguyen <miko.monday@gmail.com>
2018-05-21 20:36:07 -0700

    Added basic nav and styles

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 export default class Navigation extends React.Component {
   state = {};
 
   render() {
     return (
-      <div>Hello World</div>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            React-Hue
+          </Typography>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
